6bccaaeb0a4eaa105a6d78e5ae1c0a43598267bd
Larren Kelly Dela Peña <lkdelapena@reddragonfarm.com>
2024-07-13 14:18:52 +0800

    feat: added route and login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.scss";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
 
 function App() {
-  return <></>;
+  return <>
+  <RouterProvider router={router}/>
+  </>;
 }
 
 export default App;
