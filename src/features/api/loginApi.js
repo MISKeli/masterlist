3a3a7aa74a52b89420e283135e503dfca2f6362b
Larren Kelly Dela Peña <lkdelapena@reddragonfarm.com>
2024-07-13 15:06:30 +0800

    feat: added login API

---
+++ src/features/api/loginApi.js
@@ -0,0 +1,17 @@
+import { indexApi } from "./indexApi";
+
+const loginApi = indexApi
+  .enhanceEndpoints({ addTagTypes: ["User"] })
+  .injectEndpoints({
+    endpoints: (builder) => ({
+      postLogin: builder.mutation({
+        query: (body) => ({
+          url: "/login",
+          method: "POST",
+          body,
+        }),
+      }),
+    }),
+  });
+
+  export const {usePostLoginMutation} = loginApi;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-        login
+    <divgit 
       
     </div>
   )
