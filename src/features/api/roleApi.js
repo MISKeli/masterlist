750ee689ae92e0409d0538480519b0ee2b4afd6d
Larren Kelly Dela Peña <lkdelapena@reddragonfarm.com>
2024-07-23 09:06:16 +0800

    feat: added add, update and archive role API

--- src/features/api/roleApi.js
+++ src/features/api/roleApi.js
@@ -5,13 +5,42 @@ const roleApi = indexApi
   .injectEndpoints({
     endpoints: (builder) => ({
       getRole: builder.query({
-        query: () => ({
-          url: "/role",
+        query: (params) => ({
+          url: `/role`,
           method: "GET",
-
+          params,
         }),
+        providesTags: ["role"],
+      }),
+      addRole: builder.mutation({
+        query: (body) => ({
+          url: `/role`,
+          method: "POST",
+          body: body,
+        }),
+        invalidatesTags: ["role"],
+      }),
+      updateRole: builder.mutation({
+        query: ({ id, ...body }) => ({
+          url: `/role/${id}`,
+          method: "PATCH",
+          body: body,
+        }),
+        invalidatesTags: (_, error) => (error ? [] : ["role"]),
+      }),
+      archivedRole: builder.mutation({
+        query: (id) => ({
+          url: `/role-archived/${id}`,
+          method: "PUT",
+        }),
+        invalidatesTags: (_, error) => (error ? [] : ["role"]),
       }),
     }),
   });
 
-export const { useGetRoleQuery } = roleApi;
+export const {
+  useGetRoleQuery,
+  useAddRoleMutation,
+  useArchivedRoleMutation,
+  useUpdateRoleMutation,
+} = roleApi;
